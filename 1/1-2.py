ff3e2d03ece02779b8709d8d6b7a4e5086dc328a
Casper <casperhildebrand@gmail.com>
2021-12-01 23:11:54 +0100

    Shorten solution 1.2

--- 1/1-2.py
+++ 1/1-2.py
@@ -1,13 +1,9 @@
 with open("input-1.txt") as f:    
-    first = int(f.readline())
-    second = int(f.readline())
-    third = int(f.readline())
+    first, second, third = int(f.readline()), int(f.readline()), int(f.readline())
     count = 0
     for l in f:
         curr = int(l)
         if first + second + third < second + third + curr:
             count += 1
-        first = second
-        second = third
-        third = curr
+        first, second, third = second, third, curr
     print(count)
