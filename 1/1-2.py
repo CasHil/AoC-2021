d9e36ec2f7a6cded3c020a693af89736cd2c01a1
Casper <casperhildebrand@gmail.com>
2021-12-01 22:57:01 +0100

    1.2

---
+++ 1/1-2.py
@@ -0,0 +1,13 @@
+f = open("input-1.txt", "r")
+first = int(f.readline())
+second = int(f.readline())
+third = int(f.readline())
+count = 0
+for l in f:
+    curr = int(l)
+    if first + second + third < second + third + curr:
+        count += 1
+    first = second
+    second = third
+    third = curr
+print(count)
